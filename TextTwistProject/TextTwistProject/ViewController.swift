d5c7a8b1e4be9598d5dab3fd02cf05e613aed059
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-13 22:42:48 -0500

    MVP but doesnt display last charcter entered in the text feild

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -14,17 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var wordTextField: UITextField!
     @IBOutlet weak var wordLabel: UILabel!
     
-    var word = Word.getRandomWord() // generates random word
-    var guessedWord = [String]() // this should hold the letters the user enters
+    var word = Word.getRandomWord()
+    var guessedWord = [String]()
     var randomWordAsArray = [String]()
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         wordTextField.delegate = self
-        // let scrambled = word.scrambled
         wordLabel.text = word.scrambled
         
     }
@@ -36,67 +33,40 @@ extension ViewController: UITextFieldDelegate {
         
         guard var scrambledWord = wordLabel.text else {
             return false } // so this gives us the word scrambled in the same manner as initially presented to user
-        print("This is the word scrambled: \(scrambledWord)") // ✅
         randomWordAsArray = scrambledWord.map(String.init)
         
         let randomWord = word.unscrambled
-        print("This is the random word: \(randomWord)")
         
-        if randomWordAsArray.isEmpty {
-            view.backgroundColor = .red
-            return false
-        }
-        
-        
-        // finds the first instance of the entered letter and removes it from the scrambled word
         if randomWordAsArray.contains(string) {
             
             let letterIndex = randomWordAsArray.firstIndex(of: string)
             randomWordAsArray.remove(at: letterIndex ?? 2)
-            
             scrambledWord = randomWordAsArray.joined(separator:"")
             wordLabel.text = scrambledWord
             guessedWord.append(string)
             
-            print("This is your guess so far: \(guessedWord)")
-            
         }
-        // this is when they delete which means that string == ""
+        
         if string == "" {
             randomWordAsArray.append(guessedWord[guessedWord.count - 1])
             scrambledWord = randomWordAsArray.joined(separator:"")
             wordLabel.text = scrambledWord
             
-            print("before removal: \(guessedWord)")
             let letterIndex = (guessedWord.count - 1)
             guessedWord.remove(at: letterIndex)
-            print("after removal: \(guessedWord)")
-            
         }
         
-        if guessedWord.joined(separator:"") == randomWord {
-            view.backgroundColor = .green
-            print("correct")
-            
+         if randomWordAsArray.isEmpty {
+            if guessedWord.joined(separator:"") == randomWord {
+                view.backgroundColor = .green
+                return false
+            } else {
+                view.backgroundColor = .red
+                return false
+            }
         }
-          
-        print(string) // returns the letter they entered.
-        // print(range)
-        print("randomeWordArray: \(randomWordAsArray)")
         
         return true
     }
 }
 
-
-
-//        for (index,char) in word.unscrambled.enumerated() {
-//            if string == char.description {
-//                // remove from word.scrambled
-//            }
-//        }
-
-//==== maybe the below isnt needed
-//if word.unscrambled.hasPrefix(string){
-//    print("\(string) is the first letter in \(word.unscrambled)")
-//}
